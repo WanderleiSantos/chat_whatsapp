80d926ba2320c8e692240aabc275fe56698e6209
Wanderlei Santos <wanderleisantosdias@gmail.com>
2018-02-23 00:29:13 -0300

    Listando Contatos

--- src/actions/AppActions.js
+++ src/actions/AppActions.js
@@ -5,7 +5,8 @@ import _ from 'lodash';
 import { 
     MODIFICA_ADICIONA_CONTATO_EMAIL, 
     ADICIONA_CONTATO_ERRO,
-    ADICIONA_CONTATO_SUCESSO 
+    ADICIONA_CONTATO_SUCESSO,
+    LISTA_CONTATO_USUARIO 
 } from './types';
 
 export const modificaAdicionaContatoEmail = (texto) => {
@@ -62,5 +63,21 @@ export const habilitaContatoInclusao = () => {
     return {
         type: ADICIONA_CONTATO_SUCESSO,
         payload: false       
+    }
+}
+
+export const contatosUsuarioFetch = () => {
+    const { currentUser } = firebase.auth();
+
+    return (dispatch) => {
+        let emailUsuario64 = b64.encode(currentUser.email);
+
+        firebase.database().ref(`/usuario_contatos/${emailUsuario64}`)
+            .on("value", snapshot => {
+                dispatch({
+                    type: LISTA_CONTATO_USUARIO,
+                    payload: snapshot.val()
+                })
+            })
     }
 }
--- src/components/Contatos.js
+++ src/components/Contatos.js
@@ -1,8 +1,46 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, ListView } from 'react-native';
+import { connect } from 'react-redux';
+import { contatosUsuarioFetch } from '../actions/AppActions';
+import _ from 'lodash';
 
-export default props => (
-    <View>
-        <Text> Contatos </Text>
-    </View>
-);
+class Contatos extends Component {
+
+    componentWillMount() {
+        this.props.contatosUsuarioFetch();
+        this.criaFonteDados(this.props.contatos);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.criaFonteDados(nextProps.contatos);
+    }
+
+    criaFonteDados( contatos ) {
+        const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+        this.fonteDados =ds.cloneWithRows(contatos)        
+    }
+
+    render() {
+        return (
+            <ListView 
+                enableEmptySections
+                dataSource={this.fonteDados}
+                renderRow={data => (
+                    <View style={{ flex: 1, padding: 20, borderBottomWidth: 1, borderBottomColor: 'grey' }} >
+                        <Text style={{ fontSize: 16 }} > {data.nome} </Text>
+                    </View>
+                
+                )}
+            />
+        );
+    }
+}
+
+mapStateToProps = state => {
+    const contatos = _.map(state.ListaContatoReducer, (val, uid) => {
+        return { ...val, uid }
+    })
+    return { contatos }
+}
+
+export default connect(mapStateToProps, { contatosUsuarioFetch })(Contatos);
